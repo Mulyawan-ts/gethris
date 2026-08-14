8f1be5148c48a200f4d0782768f64802b0a5f922
Mulyawan <fr1301@protonmail.com>
2026-08-14 09:11:52 +0700

    chore: update table employee

--- backend/database/migrations/1786673351532_create_employees_table.ts
+++ backend/database/migrations/1786673351532_create_employees_table.ts
@@ -6,6 +6,12 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('nip').notNullable().unique()
+      table.string('name').notNullable()
+      table.string('email').notNullable().unique()
+      table.string('position').notNullable() // Jabatan
+      table.enum('status', ['active', 'inactive', 'on_leave']).defaultTo('active')
+      table.date('join_date').notNullable()
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
