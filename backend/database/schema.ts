97ec570e5d33507d2407f5213b0c23b365660ff2
Mulyawan <fr1301@protonmail.com>
2026-08-14 10:02:41 +0700

    feat: run migration for leave

--- backend/database/schema.ts
+++ backend/database/schema.ts
@@ -8,18 +8,7 @@ import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { DateTime } from 'luxon'
 
 export class AuthAccessTokenSchema extends BaseModel {
-  static $columns = [
-    'abilities',
-    'createdAt',
-    'expiresAt',
-    'hash',
-    'id',
-    'lastUsedAt',
-    'name',
-    'tokenableId',
-    'type',
-    'updatedAt',
-  ] as const
+  static $columns = ['abilities', 'createdAt', 'expiresAt', 'hash', 'id', 'lastUsedAt', 'name', 'tokenableId', 'type', 'updatedAt'] as const
   $columns = AuthAccessTokenSchema.$columns
   @column()
   declare abilities: string
@@ -44,17 +33,7 @@ export class AuthAccessTokenSchema extends BaseModel {
 }
 
 export class EmployeeSchema extends BaseModel {
-  static $columns = [
-    'createdAt',
-    'email',
-    'id',
-    'joinDate',
-    'name',
-    'nip',
-    'position',
-    'status',
-    'updatedAt',
-  ] as const
+  static $columns = ['createdAt', 'email', 'id', 'joinDate', 'name', 'nip', 'position', 'status', 'updatedAt'] as const
   $columns = EmployeeSchema.$columns
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime | null
@@ -70,6 +49,29 @@ export class EmployeeSchema extends BaseModel {
   declare nip: string
   @column()
   declare position: string
+  @column()
+  declare status: string | null
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+}
+
+export class LeafSchema extends BaseModel {
+  static $columns = ['createdAt', 'employeeId', 'endDate', 'id', 'leaveType', 'reason', 'startDate', 'status', 'updatedAt'] as const
+  $columns = LeafSchema.$columns
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime | null
+  @column()
+  declare employeeId: number | null
+  @column.date()
+  declare endDate: DateTime
+  @column({ isPrimary: true })
+  declare id: number
+  @column()
+  declare leaveType: string
+  @column()
+  declare reason: string | null
+  @column.date()
+  declare startDate: DateTime
   @column()
   declare status: string | null
   @column.dateTime({ autoCreate: true, autoUpdate: true })
