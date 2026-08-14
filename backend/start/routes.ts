fc0e46944fc54b1e2a365bbeb9e9f3b97d0d0089
Mulyawan <fr1301@protonmail.com>
2026-08-14 09:28:14 +0700

    feat: add employee route

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -10,28 +10,7 @@
 import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 import { controllers } from '#generated/controllers'
+import { employeeRoutes } from '#modules/employees/routes'
 
-router.get('/', () => {
-  return { hello: 'world' }
-})
-
-router
-  .group(() => {
-    router
-      .group(() => {
-        router.post('signup', [controllers.NewAccount, 'store'])
-        router.post('login', [controllers.AccessTokens, 'store'])
-      })
-      .prefix('auth')
-      .as('auth')
-
-    router
-      .group(() => {
-        router.get('profile', [controllers.Profile, 'show'])
-        router.post('logout', [controllers.AccessTokens, 'destroy'])
-      })
-      .prefix('account')
-      .as('profile')
-      .use(middleware.auth())
-  })
-  .prefix('/api/v1')
+// Load modul routes
+employeeRoutes()
