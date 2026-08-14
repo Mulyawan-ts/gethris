091bfdbeff46defe5283b97565f03e36a6e1cf3c
Mulyawan <fr1301@protonmail.com>
2026-08-14 10:00:40 +0700

    feat: add migration for leave

---
+++ backend/database/migrations/1786676379841_create_leaves_table.ts
@@ -0,0 +1,36 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'leaves'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      
+      // Menghubungkan data cuti ke ID Karyawan
+      table
+        .integer('employee_id')
+        .unsigned()
+        .references('id')
+        .inTable('employees')
+        .onDelete('CASCADE') // Jika karyawan dihapus, data cuti ikut terhapus
+
+      table.string('leave_type').notNullable() // Contoh: 'Tahunan', 'Sakit', 'Melahirkan'
+      table.date('start_date').notNullable()    // Tanggal mulai cuti
+      table.date('end_date').notNullable()      // Tanggal selesai cuti
+      table.text('reason').nullable()            // Alasan mengajukan cuti
+      
+      // Status persetujuan: 'pending', 'approved', 'rejected'
+      table
+        .enum('status', ['pending', 'approved', 'rejected'])
+        .defaultTo('pending')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
