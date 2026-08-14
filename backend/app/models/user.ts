a69bc69e0d815810383b07769c49534088d578ca
Mulyawan <fr1301@protonmail.com>
2026-08-14 11:24:01 +0700

    feat: update models user

--- backend/app/models/user.ts
+++ backend/app/models/user.ts
@@ -1,18 +1,39 @@
-import { UserSchema } from '#database/schema'
+import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
-import { compose } from '@adonisjs/core/helpers'
-import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
-import { type AccessToken, DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import Employee from '#modules/employees/models/employee'
 
-export default class User extends compose(UserSchema, withAuthFinder(hash)) {
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare employeeId: number | null
+
+  @column()
+  declare fullName: string | null
+
+  @column()
+  declare email: string
+
+  @column({ serializeAs: null }) // Password disembunyikan saat JSON dirender
+  declare password: string
+
+  @column()
+  declare role: 'admin' | 'hr' | 'employee'
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+
+  // Relasi ke Model Employee
+  @belongsTo(() => Employee)
+  declare employee: BelongsTo<typeof Employee>
+
+  // Provider Access Token untuk Auth
   static accessTokens = DbAccessTokensProvider.forModel(User)
-  declare currentAccessToken?: AccessToken
-
-  get initials() {
-    const [first, last] = this.fullName ? this.fullName.split(' ') : this.email.split('@')
-    if (first && last) {
-      return `${first.charAt(0)}${last.charAt(0)}`.toUpperCase()
-    }
-    return `${first.slice(0, 2)}`.toUpperCase()
-  }
 }
