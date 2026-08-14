f5f807ede291ac62dabf8fad948439ea970ced2b
Mulyawan <fr1301@protonmail.com>
2026-08-14 13:36:45 +0700

    feat: add employee validator

---
+++ backend/app/modules/employees/validators/employee_validator.ts
@@ -0,0 +1,31 @@
+import vine from '@vinejs/vine'
+
+/**
+ * Validator untuk Tambah Karyawan Baru
+ */
+export const createEmployeeValidator = vine.compile(
+  vine.object({
+    fullName: vine.string().trim().minLength(3).maxLength(255),
+    email: vine.string().email().trim(),
+    phoneNumber: vine.string().trim().optional(),
+    position: vine.string().trim(),
+    department: vine.string().trim(),
+    salary: vine.number().positive(),
+    joinDate: vine.string().trim(), // Format tanggal string: YYYY-MM-DD
+  })
+)
+
+/**
+ * Validator untuk Update Data Karyawan
+ */
+export const updateEmployeeValidator = vine.compile(
+  vine.object({
+    fullName: vine.string().trim().minLength(3).maxLength(255).optional(),
+    email: vine.string().email().trim().optional(),
+    phoneNumber: vine.string().trim().optional(),
+    position: vine.string().trim().optional(),
+    department: vine.string().trim().optional(),
+    salary: vine.number().positive().optional(),
+    joinDate: vine.string().trim().optional(),
+  })
+)
