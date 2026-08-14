8c44409dadd7a257c985267f535a80d4a178f7f8
Mulyawan <fr1301@protonmail.com>
2026-08-14 09:47:58 +0700

    chore: use pnpm format

--- backend/app/modules/employees/routes.ts
+++ backend/app/modules/employees/routes.ts
@@ -2,11 +2,13 @@ import router from '@adonisjs/core/services/router'
 const EmployeesController = () => import('./controllers/employees_controller.js')
 
 export function employeeRoutes() {
-  router.group(() => {
-    router.get('/', [EmployeesController, 'index'])
-    router.get('/:id', [EmployeesController, 'show'])
-    router.post('/', [EmployeesController, 'store'])
-    router.put('/:id', [EmployeesController, 'update'])
-    router.delete('/:id', [EmployeesController, 'destroy'])
-  }).prefix('/api/employees')
+  router
+    .group(() => {
+      router.get('/', [EmployeesController, 'index'])
+      router.get('/:id', [EmployeesController, 'show'])
+      router.post('/', [EmployeesController, 'store'])
+      router.put('/:id', [EmployeesController, 'update'])
+      router.delete('/:id', [EmployeesController, 'destroy'])
+    })
+    .prefix('/api/employees')
 }
