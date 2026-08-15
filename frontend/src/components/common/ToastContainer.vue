b81da7d7fb7a7d6c32271c91af0fa5edae070ac4
Mulyawan <fr1301@protonmail.com>
2026-08-15 13:40:15 +0700

    feat: add toast container

---
+++ frontend/src/components/common/ToastContainer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import BaseToast from './BaseToast.vue'
+import { useToast } from '@/composables/useToast'
+
+const { toasts, removeToast } = useToast()
+</script>
+
+<template>
+  <div class="fixed top-5 right-5 z-50 flex flex-col gap-3 pointer-events-none w-full max-w-sm">
+    <BaseToast
+      v-for="toast in toasts"
+      :key="toast.id"
+      :type="toast.type"
+      :title="toast.title"
+      :message="toast.message"
+      :show="true"
+      @close="removeToast(toast.id!)"
+    />
+  </div>
+</template>
